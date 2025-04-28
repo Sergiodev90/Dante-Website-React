8cf178a2665be79c1246fde6855f1a6e80cc6e46
sergio noriega <sergionoriegacantillo@gmail.com>
2025-04-28 18:29:03 +0200

    draggable button

---
+++ src/AppUI.jsx
@@ -0,0 +1,58 @@
+import React, {useState} from 'react';
+import {App} from './pages/App';
+import {DndContext} from '@dnd-kit/core';
+import { DraggableStory } from './components/DraggableStory';
+import { Layout } from './layout'
+
+
+import './App.css'
+
+function AppUI() {
+  const [parent, setParent] = useState(null);
+  const draggable = (
+    <DraggableStory id="draggable">
+      Go ahead, drag me.
+    </DraggableStory>
+  );
+  return (
+    <>
+
+      <Layout>
+         <App/>
+             <DndContext onDragEnd={handleDragEnd}>
+             {!parent ? draggable : null}
+      </DndContext>
+
+      </Layout>
+
+    </>
+  );
+  function handleDragEnd({over}) {
+    setParent(over ? over.id : null);
+  }
+}
+
+// function Example() {
+//   const [parent, setParent] = useState(null);
+//   const draggable = (
+//     <Draggable id="draggable">
+//       Go ahead, drag me.
+//     </Draggable>
+//   );
+
+//   return (
+//     <DndContext onDragEnd={handleDragEnd}>
+//       {!parent ? draggable : null}
+//       <Droppable id="droppable">
+//         {parent === "droppable" ? draggable : 'Drop here'}
+//         <AppUI/>
+//       </Droppable>
+//     </DndContext>
+//   );
+
+//   function handleDragEnd({over}) {
+//     setParent(over ? over.id : null);
+//   }
+// }
+
+export  {AppUI};
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,12 @@
+function Home (){
+
+    return(
+        <>
+            <div>Carepoto</div>
+        </>
+    );
+
+}
+
+
+export  {Home};
